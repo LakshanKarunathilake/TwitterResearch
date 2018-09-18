0db90be417b9cc9b26218fa47505d1f7d8528fa4
BurbaCodes <dilushadasanayaka@gmail.com>
2018-09-18 22:23:30 +0530

    add stacked bar chart on tweets

--- src/pages/detailed-report/detailed-report.ts
+++ src/pages/detailed-report/detailed-report.ts
@@ -1,5 +1,5 @@
 import { Observable } from "rxjs/Observable";
-import { Component } from "@angular/core";
+import { Component ,ViewChild } from "@angular/core";
 import {
   IonicPage,
   NavController,
@@ -11,6 +11,8 @@ import {
 import { DetailedReport } from "./detailedRepoGen";
 import { AngularFirestore } from "angularfire2/firestore";
 import { Tweet_Sentiment } from "../../models/SentimentModels/Tweet_Sentiment";
+
+import {Chart } from 'chart.js'
 
 import { Sentiment } from "../../models/SentimentModels/Sentiment";
 
@@ -36,6 +38,7 @@ export class DetailedReportPage {
   shownGroup = null;
   desctiptionSentiment = null;
   replySentiment = null;
+  barchartData = [];
 
   emojiiToggle = false;
   emojiis = [];
@@ -56,6 +59,8 @@ export class DetailedReportPage {
     this.generateReport();
   }
 
+  @ViewChild('BarCanvas') BarCanvas;
+
   generateReport() {
     this.presentLoading();
     this.repo_gen.getTweets(this.user_data).then(data => {
@@ -63,11 +68,89 @@ export class DetailedReportPage {
       array_of_tweets = JSON.parse(JSON.stringify(data));
 
       console.log("array of tweets", array_of_tweets);
+      let x = 0;
+this.barchartData['datasets'] = [];
+this.barchartData['labels'] = [];
+this.barchartData['labels'][x]=[];
+
+
+
+this.barchartData['datasets']=[];
+
+this.barchartData['datasets'][0]=[];
+this.barchartData['datasets'][1]=[];
+this.barchartData['datasets'][2]=[];
+
+this.barchartData['datasets'][0]['data']=[]; 
+this.barchartData['datasets'][1]['data']=[];
+this.barchartData['datasets'][2]['data']=[];
+
+let backgroundColorRed = [
+                        'rgba(255, 0, 0, 0.3)',
+                        'rgba(255, 0, 0, 0.3)',
+                        'rgba(255, 0, 0, 0.3)'
+                      ];
+                      let backgroundColorGreen = [
+                        'rgba(0, 255, 0, 0.3)',
+                        'rgba(0, 255, 0, 0.3)',
+                        'rgba(0, 255, 0, 0.3)'
+                      ];
+
+                      let backgroundColorBlue = [
+                        'rgba(0, 0, 255, 0.3)',
+                        'rgba(0, 0, 255, 0.3)',
+                        'rgba(0, 0, 255, 0.3)'
+                      ];
+
+this.barchartData['datasets'][0]['backgroundColor'] = [];
+this.barchartData['datasets'][1]['backgroundColor'] = [];
+this.barchartData['datasets'][2]['backgroundColor'] = [];
+
+
+this.barchartData['datasets'][0]['backgroundColor'] = backgroundColorRed;
+this.barchartData['datasets'][1]['backgroundColor'] = backgroundColorGreen;
+this.barchartData['datasets'][2]['backgroundColor'] = backgroundColorBlue;
+
+      for(let x =0;x<array_of_tweets.length; x++){
+        // this.barchartData['labels'][x]=[];
+        this.barchartData['labels'][x] = 'Tweet '+(x+1); 
+
+
+        // this.barchartData['datasets']=[];
+
+        // this.barchartData['datasets'][0]=[];
+        // this.barchartData['datasets'][1]=[];
+        // this.barchartData['datasets'][2]=[];
+
+        this.barchartData['datasets'][0]['label'] = 'Anger ';
+        this.barchartData['datasets'][1]['label'] = 'Fear ';
+        this.barchartData['datasets'][2]['label'] = 'Joy ';
+
+        // this.barchartData['datasets'][0]=[];
+        // this.barchartData['datasets'][1]=[];
+        // this.barchartData['datasets'][2]=[];
+
+        // this.barchartData['datasets'][0]['data']=[]; 
+        // this.barchartData['datasets'][1]['data']=[];
+        // this.barchartData['datasets'][2]['data']=[];
+
+        this.barchartData['datasets'][0]['data'][x]=[];
+
+        this.barchartData['datasets'][0]['data'][x] = array_of_tweets[x].sentiment.Anger; 
+        this.barchartData['datasets'][1]['data'][x] = array_of_tweets[x].sentiment.Fear;
+        this.barchartData['datasets'][2]['data'][x] = array_of_tweets[x].sentiment.Joy;
+        
+      }
+
+      console.log("bar chart data",this.barchartData);
+      
+      this.drawBarChart(this.barchartData);
+
+
       this.tweets_observable = array_of_tweets.map(val => {
         val.description = val.description;
         val.doc_id = val.doc_id;
         val.sentiment = val.sentiment;
-
         this.repo_gen
           .getReplySentiment({ tweet_id: val.doc_id })
           .then((re: Sentiment) => {
@@ -85,6 +168,8 @@ export class DetailedReportPage {
 
         return val;
       });
+
+      //console.log(this.tweets_observable);
     });
     this.hideLoading();
   }
@@ -193,5 +278,23 @@ export class DetailedReportPage {
 
   hideLoading() {
     this.loading.dismiss();
+  }
+
+  drawBarChart(data){
+    var stackedBar = new Chart(this.BarCanvas.nativeElement, {
+      type: 'bar',
+      data: data,
+      options: {
+          scales: {
+              xAxes: [{
+                  stacked: true
+              }],
+              yAxes: [{
+                  stacked: true
+              }]
+          }
+      }
+  });
+
   }
 }
